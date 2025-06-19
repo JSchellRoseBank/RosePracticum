fc5b19bcfe3fafe05daf625d92065e129e755c66
JSchellRoseBank <st10486426@rcconnect.edu.za>
2025-06-19 11:18:56 +0200

    add btns on detailed screen

--- app/src/main/java/com/example/myapplication/DetailedViewScreen.kt
+++ app/src/main/java/com/example/myapplication/DetailedViewScreen.kt
@@ -3,8 +3,10 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,7 +19,19 @@ class DetailedViewScreen : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
+                Column {
+                    Button(onClick = {}) {
+                        Text(text = "Show Playlist")
+                    }
 
+                    Button(onClick = {}) {
+                        Text(text = "Show average Rating for playlist")
+                    }
+
+                    Button(onClick = {}) {
+                        Text(text = "Back")
+                    }
+                }
             }
         }
     }
