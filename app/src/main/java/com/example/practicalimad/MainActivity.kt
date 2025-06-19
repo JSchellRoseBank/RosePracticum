878eabba6783ccc22fde8072c2915d6d56824ca9
JSchellRoseBank <st10486426@rcconnect.edu.za>
2025-06-19 10:03:18 +0200

    add btn for playlist

--- app/src/main/java/com/example/practicalimad/MainActivity.kt
+++ app/src/main/java/com/example/practicalimad/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.practicalimad
 
+import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.practicalimad.ui.theme.PracticalIMADTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,10 +36,49 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var song = mutableListOf<String>()
+            var artist = mutableListOf("")
+            var ratings = mutableListOf("")
+            var comments = mutableListOf("")
+            var addToPlaylist by remember { mutableStateOf(false) }
+
             PracticalIMADTheme {
-                
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 30.dp, top = 60.dp, end = 30.dp)
+                ){
+                    Text(text="testing")
+                    Row {
+                        Button(onClick = {
+                            addToPlaylist = true
+                        }) { Text(text = "Add To Playlist") }
+                    }
+                    if(addToPlaylist) {
+                        Row {
+                            OutlinedTextField(
+                                modifier = Modifier
+                                    .clip(RoundedCornerShape(12.dp))
+                                    .border(1.dp, Color.Gray, shape = RoundedCornerShape(12.dp)),
+                                textStyle = TextStyle(color = Color.Black),
+                                value = song,
+                                onValueChange = {
+                                        text ->
+                                    song.add(text)
+                                },
+                                placeholder = {
+                                    Text(text = "Enter Time Of Day Here")
+                                },
+                                keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences)
+                            )
+                        }
+                    }
+                }
             }
         }
     }
 }
 
+@Composable
+fun 
